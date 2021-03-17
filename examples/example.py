9e4a5e0a7eb84fad3b6939c5f8715b15b6a630ea
Philippe Remy <premy.enseirb@gmail.com>
2021-03-17 12:41:21 +0900

    0.4

--- examples/example.py
+++ examples/example.py
@@ -6,10 +6,10 @@ rakuma_api = Rakuma()
 
 print('_' * 80)
 print(mercari_api.name)
-print(mercari_api.fetch_all_items(keyword='CHANEL'))
+print(mercari_api.fetch_all_items(keyword='CHANEL')[0:10])
 print(mercari_api.get_item_info('https://www.mercari.com/jp/items/m88046246209/'))
 
 print('_' * 80)
 print(rakuma_api.name)
-print(rakuma_api.fetch_all_items(keyword='CHANEL'))
+print(rakuma_api.fetch_all_items(keyword='CHANEL')[0:10])
 print(rakuma_api.get_item_info('https://item.fril.jp/e0c79971ed2b15e083428d93803e78f0'))
--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
-from distutils.core import setup
+from setuptools import find_packages
+from setuptools import setup
 
-setup(name='mercari-python',
-      version='0.1',
-      author='Philippe Remy',
-      packages=['mercari'],
-      zip_safe=False)
+setup(
+    name='mercari-python',
+    version='0.4',
+    author='Philippe Remy',
+    packages=find_packages(),
+    install_requires=[
+        'mailthon',
+        'requests',
+        'beautifulsoup4',
+        'lxml',
+        'wget'
+    ]
+)
